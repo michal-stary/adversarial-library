38fb816ee3df8b8692b451317f07a2ecb2b26fd5
Michal Stary <michalstary98@gmail.com>
2022-05-31 14:25:19 +0200

    add fmn2

--- adv_lib/attacks/__init__.py
+++ adv_lib/attacks/__init__.py
@@ -4,6 +4,7 @@ from .carlini_wagner import carlini_wagner_l2, carlini_wagner_linf
 from .decoupled_direction_norm import ddn
 from .fast_adaptive_boundary import fab
 from .fast_minimum_norm import fmn
+from .fast_minimum_norm2 import fmn2
 from .perceptual_color_attacks import perc_al
 from .primal_dual_gradient_descent import pdgd, pdpgd
 from .projected_gradient_descent import pgd_linf
